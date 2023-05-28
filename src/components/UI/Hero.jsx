61ac1facb7d56cf8d3139134286db7c15245e38f
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-05-28 22:33:45 +0200

    feat: finish Hero section

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -54,8 +54,111 @@ const Hero = () => {
                             elit. Neque commodi at maiores quod, repudiandae
                             sunt vero dicta obcaecati nihil similique!
                         </p>
+
+                        <div className="flex items-center gap-9 mt-14">
+                            <span className="text-smallTextColor text-[15px] font-[600]">
+                                Follow me:
+                            </span>
+                            <span>
+                                <a
+                                    href="#"
+                                    className="text-smallTextColor text-[18px] font-[600]"
+                                >
+                                    <i className="ri-youtube-line"></i>
+                                </a>
+                            </span>
+                            <span>
+                                <a
+                                    href="#"
+                                    className="text-smallTextColor text-[18px] font-[600]"
+                                >
+                                    <i className="ri-github-fill"></i>
+                                </a>
+                            </span>
+                            <span>
+                                <a
+                                    href="#"
+                                    className="text-smallTextColor text-[18px] font-[600]"
+                                >
+                                    <i className="ri-facebook-line"></i>
+                                </a>
+                            </span>
+                            <span>
+                                <a
+                                    href="#"
+                                    className="text-smallTextColor text-[18px] font-[600]"
+                                >
+                                    <i className="ri-instagram-line"></i>
+                                </a>
+                            </span>
+                        </div>
                     </div>
                     {/*========= HERO LEFT CONTENT END =========*/}
+
+                    {/*========= HERO IMAGE =========*/}
+                    <div className="basis-1/3 mt-10 sm:mt-0">
+                        <figure className="flex items-center justify-center">
+                            <img src={heroImg} alt="" />
+                        </figure>
+                    </div>
+                    {/*========= HERO IMAGE END =========*/}
+
+                    {/*========= HERO RIGHT CONTENT =========*/}
+                    <div className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
+                        <div className="mb-10">
+                            <h2 className="text-headingColor font-[700] text-[32px]">
+                                <CountUp
+                                    start={0}
+                                    end={6}
+                                    duration={2}
+                                    suffix="+"
+                                />
+                            </h2>
+                            <h4 className="text-headingColor font-[600] text-[18px]">
+                                Years of experience
+                            </h4>
+                        </div>
+                        <div className="mb-10">
+                            <h2 className="text-headingColor font-[700] text-[32px]">
+                                <CountUp
+                                    start={0}
+                                    end={100}
+                                    duration={2}
+                                    suffix="%"
+                                />
+                            </h2>
+                            <h4 className="text-headingColor font-[600] text-[18px]">
+                                Success rate
+                            </h4>
+                        </div>
+                        <div className="mb-10">
+                            <h2 className="text-headingColor font-[700] text-[32px]">
+                                <CountUp
+                                    start={0}
+                                    end={150}
+                                    duration={2}
+                                    suffix="+"
+                                />
+                            </h2>
+                            <h4 className="text-headingColor font-[600] text-[18px]">
+                                Happy clients
+                            </h4>
+                        </div>
+                        <div className="mb-10">
+                            <h2 className="text-headingColor font-[700] text-[32px]">
+                                <CountUp
+                                    start={0}
+                                    end={249}
+                                    duration={2}
+                                    suffix="+"
+                                />
+                            </h2>
+                            <h4 className="text-headingColor font-[600] text-[18px]">
+                                Projects Completed
+                            </h4>
+                        </div>
+                    </div>
+                    {/*========= HERO RIGHT CONTENT END =========*/}
                 </div>
             </div>
         </section>
