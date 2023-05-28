0c4931ffa61d6e4d26f122d2ff551dfb417cfccb
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-05-28 17:08:12 +0200

    feat: start working on portfolio and finish logo section

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import Aos from "aos";
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+    useEffect(() => {
+        Aos.init();
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
